caa77963fa2d67d5d25a40543b3759b7ecdec981
ANKIT RAJ <110198496+ankittraj@users.noreply.github.com>
2025-05-18 10:44:59 +0530

    Add files via upload

---
+++ DM1.java
@@ -0,0 +1,17 @@
+package DecisionMaking;
+
+public class DM1 {
+
+	public static void main(String[] args) {
+		if(2<3)
+			System.out.println("hello");
+		else 
+			System.out.println("hi");
+		
+		if(false)
+			System.out.println("hello");
+		else 
+			System.out.println("hi");
+	}
+
+}
