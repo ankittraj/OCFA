e23f227cc58f8418a7c9660e39ab664f210779d8
ANKIT RAJ <110198496+ankittraj@users.noreply.github.com>
2025-05-19 08:26:54 +0530

    Add files via upload

---
+++ DM4.java
@@ -0,0 +1,19 @@
+package DecisionMaking;
+
+public class DM4 {
+    // This program demonstrates the use of if-else statements with block statements.
+	public static void main(String[] args) {
+		if(2<3){
+			int b=5;
+			System.out.println(b);
+		}else {
+			int b=6;
+			System.out.println(b);
+		}
+		if(3<5) {
+			int b=7;
+			System.out.println(b);
+		}
+	}
+
+}
