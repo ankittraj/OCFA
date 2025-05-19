5039bce892d5417df99f9bba06fabc051277f558
ANKIT RAJ <110198496+ankittraj@users.noreply.github.com>
2025-05-19 08:26:38 +0530

    Add files via upload

---
+++ DM3.java
@@ -0,0 +1,16 @@
+package DecisionMaking;
+
+public class DM3 {
+
+	public static void main(String[] args) {
+		int a=10;
+		if(a++<11){
+			int b=5;
+			b++;
+			System.out.println("Hello"+b);
+		}
+		int b=9;
+		System.out.println("Bye"+a+b);
+	}
+
+}
