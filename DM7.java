fe8d2fc3ed4c24d3c1de09526f92af4e869f5454
ANKIT RAJ <110198496+ankittraj@users.noreply.github.com>
2025-05-22 12:50:06 +0530

    Add files via upload

---
+++ DM7.java
@@ -0,0 +1,12 @@
+package DecisionMaking;
+
+public class DM7 {
+	public static void main(String[] args) {
+		final int j;
+		j=9;
+		switch(9) {
+			//case j: System.out.println("hello"); //error
+		}
+	}
+
+}
