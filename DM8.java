3690a8fdd5b311e65f61e5026aae783534585686
ANKIT RAJ <110198496+ankittraj@users.noreply.github.com>
2025-05-22 12:50:21 +0530

    Add files via upload

---
+++ DM8.java
@@ -0,0 +1,13 @@
+package DecisionMaking;
+
+public class DM8 {
+	public static void main(String[] args) {
+		int n=9;
+		switch(n) {
+			// ; //error
+			case 2: System.out.println("Hi"); break;
+			// ; //error
+		}
+	}
+
+}
