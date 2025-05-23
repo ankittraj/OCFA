1961dd088db5c4889262dad822b95d8048cade82
ANKIT RAJ <110198496+ankittraj@users.noreply.github.com>
2025-05-23 11:06:24 +0530

    Add files via upload

---
+++ BooleanLiteral.java
@@ -0,0 +1,14 @@
+public class BooleanLiteral {
+    public static void main(String[] args) {
+        boolean a=true; //Boolean Lietral
+        boolean b=false; //Boolean Lietral
+        //boolean a=1; //error
+        //boolean a=0; //error
+        //boolean a=True; //error
+        //boolean a=TRUE; //error
+        //boolean a='true'; //error
+        //boolean a="true"; //error
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
